9a3fdc4b0efe76f715ee060da3ff465908cea5c4
jelf <353742991@qq.com>
2018-01-15 17:34:06 +0800

    feat:bottom-nav组件和页面json数据

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-
+    <component :key="index" v-for="(data,index) in shopPageData" :data="data.data" :is="data.component"></component>
   </div>
 </template>
 
 <script>
+  import shopPageData from './pages/shop'
+  import {JBottomNav, JSearch} from '../index'
+
   export default {
     name: 'app',
+    components: {JBottomNav, JSearch},// 这里可以一次性把所有组件导入，也可以选择部分导入
     data() {
-      return {}
+      return {
+        shopPageData
+      }
     }
   }
 </script>
