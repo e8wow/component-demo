7184c96d8729aa9ff85172508f61759877a6c62e
jelf <353742991@qq.com>
2018-01-15 16:58:39 +0800

    初始化导入

---
+++ src/components/nav/nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="prefixCls">
+    <div :class="" v-for="s in data.son">{{s}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "bottom-nav",
+    props: {
+      prefixCls: {
+        type: String,
+        default: 'bottom-nav'
+      },
+      data: {
+        type: Object
+      }
+    },
+    computed: {
+      sonClasss() {
+        return [
+          `${this.prefixCls}-son`
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  $prefixCls: 'bottom-nav';
+
+  $prefixCls{
+
+  }
+</style>
